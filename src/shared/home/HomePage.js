02afff617600568770d49cefba680a76481f6960
Alexis Janvier <contact@alexisjanvier.net>
2017-07-16 12:30:03 +0200

    Add Date-fns on homepage

--- src/shared/home/HomePage.js
+++ src/shared/home/HomePage.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import format from 'date-fns/format';
+import locale from 'date-fns/locale/fr';
 
 const HomePage = () => (
-    <h1>Homepage</h1>
+    <div>
+        <h1>Homepage</h1>
+        <p>{format(new Date(), 'dddd DD MMMM YYYY', { locale })}</p>
+    </div>
 );
 
 export default HomePage;
